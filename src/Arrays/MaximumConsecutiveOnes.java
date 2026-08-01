4769d5f8768bae1c4f7bedd4f3303352326e4da1
Shivam Gaikwad <gaikwadshivam572@gmail.com>
2026-08-01 22:30:39 +0530

    more array problem's

---
+++ src/Arrays/MaximumConsecutiveOnes.java
@@ -0,0 +1,28 @@
+package Arrays;
+public class MaximumConsecutiveOnes {
+
+    public static int findMaxConsecutiveOnes(int[] arr) {
+
+        int count = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 0, 1, 1, 1};
+
+        System.out.println("Maximum Consecutive Ones: " + findMaxConsecutiveOnes(arr));
+    }
+}
